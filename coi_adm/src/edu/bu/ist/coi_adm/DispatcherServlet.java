4dbb67a8fc0b96e4264d5adaab0870b4258015c0
Sergey Tetelbaum <stetelba@bu.edu>
2015-05-22 14:20:04 -0400

    Minor cleanup to eliminate compiler warnings

--- coi_adm/src/edu/bu/ist/coi_adm/DispatcherServlet.java
+++ coi_adm/src/edu/bu/ist/coi_adm/DispatcherServlet.java
@@ -1,32 +1,15 @@
 package edu.bu.ist.coi_adm;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
-import org.apache.catalina.connector.Request;
 import org.apache.log4j.Logger;
-
-import com.opencsv.CSVReader;
-import edu.bu.ist.coi.db._EmployeeListAdditionsId;
 
 @WebServlet(name = "DispatcherServlet", urlPatterns = { "/dispatcher" })
 @MultipartConfig
